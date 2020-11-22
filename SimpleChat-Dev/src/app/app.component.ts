f1f59a807c10317b2481d411ff7fa31357c9e2aa
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2020-11-22 21:28:15 +0800

    Final Submission

--- SimpleChat-Dev/src/app/app.component.ts
+++ SimpleChat-Dev/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDd9oIY5m4FRScmj9qTJenXHxGn3C-wjKA',
+  databaseURL: 'https://chatwithstrangers-69c9f.firebaseio.com'
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SimpleChat-Dev';
+  title = 'ChatwithStrangers';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
